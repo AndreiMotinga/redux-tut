47e7dcbc8cd35becbec5cf72c6c46c9dc949b931
Andrei Motinga <andrei@Andreis-MacBook-Air.local>
2017-02-09 13:24:18 -0500

    build createStore form scratch

--- src/index.js
+++ src/index.js
@@ -3,33 +3,30 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import expect from 'expect';
-import { createStore } from 'redux'
+// import { createStore } from 'redux'
 
 ReactDOM.render(
   <App />,
   document.getElementById("root")
 );
 
-const counter = (state = 0, action) => {
-  switch(action.type) {
-    case('INCREMENT'):
-      return state + 1;
-    case('DECREMENT'):
-      return state - 1;
-    default:
-      return state;
-  }
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    };
+  };
+
+  return { getState, dispatch, subscribe };
 }
-
-const store = createStore(counter);
-
-const render = () => {
-  document.getElementById("redux").innerText = store.getState();
-}
-
-store.subscribe(render);
-render();
-
-document.addEventListener("click", () => {
-  store.dispatch({ type: "INCREMENT" });
-});
